61927929c6b8706230b054f536ec4f47c44fe7db
mbennett <mbennett5516@yahoo.com>
2018-11-05 10:28:27 -0600

    get account function working

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-$('#home').on('click', function(){
+const getAccountBalance = function(){
     $.ajax({
         url: '/api/account',
         method: "GET",
@@ -7,6 +7,16 @@ $('#home').on('click', function(){
         for (let i = 0; i < response.length; i++){
             sum += response[i].amount;
         }
-        $('#account').text(sum);
+        if(sum > 0){
+            $('#account').css('background-color', '#19722e');
+        $('#account').html(`<div class="card-body"><h3>Account Balance: $${sum.toFixed(2)}</h3></div>`);
+        }
+        else{
+            $('#account').css('background-color', 'red');
+            $('#account').html(`<div class="card-body"><h3>Account Balance: $${sum.toFixed(2)}</h3></div>`);
+        }
     })
-})
+}
+
+$('#home').on('click', getAccountBalance);
+$('.navbar-brand').on('click', getAccountBalance);
